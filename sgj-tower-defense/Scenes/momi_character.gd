b1356c527a7e7da429d93bd1238dea2bf0138c2c
i0n0x1 <i0n0x1@proton.me>
2026-08-30 01:19:32 +0200

    added mom death particles

--- sgj-tower-defense/Scenes/momi_character.gd
+++ sgj-tower-defense/Scenes/momi_character.gd
@@ -1,9 +1,16 @@
 extends CharacterBody2D
 
 @export var Health_Component: HealthComponent
+@export var particle_mom_deathScene: PackedScene
 
 func _ready() -> void:
 	Health_Component.connect("health_below_zero", _on_momi_death)
 
 func _on_momi_death() -> void:
+	
+	#spawn particles
+	var particle = particle_mom_deathScene.instantiate()
+	get_tree().current_scene.add_child(particle)
+	particle.initialize(global_position)
+	
 	queue_free()
