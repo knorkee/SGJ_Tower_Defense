bad0b3922593033df5128885560e3a3b02278812
i0n0x1 <i0n0x1@proton.me>
2026-08-29 12:35:45 +0200

    added random enemy spawners

--- sgj-tower-defense/Scenes/enemy_spawner.gd
+++ sgj-tower-defense/Scenes/enemy_spawner.gd
@@ -2,8 +2,18 @@ extends Node2D
 @export var enemy_prefab : PackedScene
 @export var Target : Node2D
 @export var Player : Node2D
+@export var min_x: int
+@export var max_x: int
+@export var min_y: int
+@export var max_y: int
+func _generate_random_pos(min_x,max_x,min_y,max_y):
+	var rng = RandomNumberGenerator.new()
+	var x = rng.randi_range(min_x, max_x)
+	var y = rng.randi_range(min_y, max_y)
+	return Vector2(x,y)
 
 func _on_timer_timeout() -> void:
 	var enemy = enemy_prefab.instantiate()
 	add_child(enemy)
-	enemy.target = Target.global_position	  
+	enemy.target = Target.global_position
+	enemy.position = _generate_random_pos(min_x,max_x,min_y,max_y)
