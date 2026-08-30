d7b5b568657dd803294dbbc5822c1a3a9413b813
Vaku-art <vaku-online@pm.me>
2026-08-30 15:51:52 +0200

    credit screen in game now

--- sgj-tower-defense/Scenes/main_menu.gd
+++ sgj-tower-defense/Scenes/main_menu.gd
@@ -18,3 +18,7 @@ func _on_options_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/controls_screen.tscn")
