947a593c16bfa77a56edaf5249fbf8587efdd2a3
Kursatayverdi <kuersat.ayverdi@st.oth-regensburg.de>
2026-08-30 09:59:04 +0200

    added beautiful death sound
    trust me, beautiful

--- sgj-tower-defense/Scenes/momi_character.gd
+++ sgj-tower-defense/Scenes/momi_character.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var Health_Component: HealthComponent
 @export var particle_mom_deathScene: PackedScene
+@onready var audio_stream_player_death: AudioStreamPlayer = $"../AudioStreamPlayerDeath"
 
 func _ready() -> void:
 	Health_Component.connect("health_below_zero", _on_momi_death)
@@ -12,5 +13,6 @@ func _on_momi_death() -> void:
 	var particle = particle_mom_deathScene.instantiate()
 	get_tree().current_scene.add_child(particle)
 	particle.initialize(global_position)
+	audio_stream_player_death.play()
 	
 	queue_free()
