48bf2c3ca707c51ee97c4ccc674c051209e95b77
Kursatayverdi <kuersat.ayverdi@st.oth-regensburg.de>
2026-08-30 02:38:47 +0200

    added wave management to update health
    health * wave

--- sgj-tower-defense/Scenes/enemy_spawner.gd
+++ sgj-tower-defense/Scenes/enemy_spawner.gd
@@ -14,10 +14,12 @@ func _generate_random_pos(min_x,max_x,min_y,max_y):
 	return Vector2(x,y)
 func _spawnEnemy() -> void:
 		var enemy = enemy_prefab.instantiate()
+		var healthcomp = enemy.get_node("HealthComponent")
+		healthcomp.hitpoints = healthcomp.hitpoints * game_scene.waveCounter
 		add_child(enemy)
 		enemy.target = Target.global_position
 		enemy.position = _generate_random_pos(min_x,max_x,min_y,max_y)
-
+		
 'func _on_timer_timeout() -> void:
 	var enemy = enemy_prefab.instantiate()
 	add_child(enemy)
