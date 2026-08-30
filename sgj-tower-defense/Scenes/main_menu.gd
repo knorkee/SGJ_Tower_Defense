90c4848571045c9e727c58c8c856f0015315766e
i0n0x1 <i0n0x1@proton.me>
2026-08-30 07:11:31 +0200

    added btn graphics

--- sgj-tower-defense/Scenes/main_menu.gd
+++ sgj-tower-defense/Scenes/main_menu.gd
@@ -10,14 +10,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/story1.tscn")
 
-
 func _on_options_pressed() -> void:
 	pass # Replace with function body.
-
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
