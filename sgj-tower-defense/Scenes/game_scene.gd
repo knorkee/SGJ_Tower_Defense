8f66d9c74ae7ec68d5091d2a02c8f5fc3e092095
i0n0x1 <i0n0x1@proton.me>
2026-08-30 11:15:49 +0200

    added fake stats upgrade

--- sgj-tower-defense/Scenes/game_scene.gd
+++ sgj-tower-defense/Scenes/game_scene.gd
@@ -5,6 +5,11 @@ extends Node2D
 @onready var next_wave_button: TextureButton = $NextWaveButton
 @onready var game_over_screen: Node2D = $GameOverScreen
 
+@onready var dmg: Label = $ui_game/box/line_top/stats_box/dmg_number
+@onready var _const: Label = $ui_game/box/line_top/stats_box/const_number
+@onready var floppynes: Label = $ui_game/box/line_top/stats_box/floppynes_number
+
+@export var hit_indicator: PackedScene
 
 var enemyCounter: int
 var waveCounter: int = 1
@@ -21,12 +26,47 @@ func _prepareEnemies() -> void:
 	enemyCounter = waveCounter * 10	
 	
 func _process(delta: float) -> void:
-	if(enemies == 0 && enemyCounter == 0):
+	if(enemies == 0 && enemyCounter == 0 && game_won == false):
 		game_won = true
+		
+		#fake upgrade
+		var mult_indicator1 = hit_indicator.instantiate()
+		var mult_indicator2 = hit_indicator.instantiate()
+		var mult_indicator3 = hit_indicator.instantiate()
+		
+		get_tree().current_scene.add_child(mult_indicator1)
+		get_tree().current_scene.add_child(mult_indicator2)
+		get_tree().current_scene.add_child(mult_indicator3)
+		
+		var tween = create_tween()
+		tween.tween_property(dmg, "scale", Vector2(1.5,1.5), 0.4)
+		tween.parallel().tween_property(dmg, "modulate", Color.RED,0.2)
+		tween.parallel().tween_property(_const, "scale", Vector2(1.5,1.5), 0.4)
+		tween.parallel().tween_property(_const, "modulate", Color.RED,0.2)
+		tween.parallel().tween_property(floppynes, "scale", Vector2(1.5,1.5), 0.4)
+		tween.parallel().tween_property(floppynes, "modulate", Color.RED,0.2)
+		
+		mult_indicator1.initialize_text_moving_down(dmg.global_position,"x1",Color.RED)
+		mult_indicator2.initialize_text_moving_down(_const.global_position,"x1",Color.RED)
+		mult_indicator3.initialize_text_moving_down(floppynes.global_position,"x1",Color.RED)
+		
+		
+		tween.tween_property(dmg, "scale", Vector2(1.0,1.0), 0.4)
+		tween.parallel().tween_property(dmg, "modulate", Color.WHITE, 0.2)
+		tween.parallel().tween_property(_const, "scale", Vector2(1.0,1.0), 0.4)
+		tween.parallel().tween_property(_const, "modulate", Color.WHITE, 0.2)
+		tween.parallel().tween_property(floppynes, "scale", Vector2(1.0,1.0), 0.4)
+		tween.parallel().tween_property(floppynes, "modulate", Color.WHITE, 0.2)
+		if(await tween.finished):
+			tween.kill()
+			
+			
+			
 	if(game_lost):
 		game_over_screen.show()
 	if(game_won):
 		next_wave_button.visible = true
+		
 		#_spawnWaveButton()
 		#_start_next_wave()
 #func _spawnWaveButton() -> void:
--- sgj-tower-defense/Scenes/hit_indicator.gd
+++ sgj-tower-defense/Scenes/hit_indicator.gd
@@ -21,6 +21,8 @@ func initialize(Position: Vector2, Damage: int) -> void:
 	if(await tween.finished):
 		tween.kill()
 	queue_free()
+	
+	
 func initialize_text(Position: Vector2, labeltext: String) -> void:
 		position = Position
 		text = labeltext
@@ -32,7 +34,17 @@ func initialize_text(Position: Vector2, labeltext: String) -> void:
 			tween.kill()
 		queue_free()
 		
+func initialize_text_moving_down(Position: Vector2, labeltext: String, color: Color) -> void:
+		position = Position
+		text = labeltext
+		color = color
 		
+		var target = Vector2(Position.x,Position.y + 30)
+		var tween = create_tween()
+		tween.tween_property(get_node("."), "position", target, 1.0 )
+		if(await tween.finished):
+			tween.kill()
+		queue_free()
 	
 	
 	
