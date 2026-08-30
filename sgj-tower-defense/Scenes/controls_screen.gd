0a9518a602122483f4915c2eed416eaca4474e56
i0n0x1 <i0n0x1@proton.me>
2026-08-30 09:21:57 +0200

    added controls screen

--- sgj-tower-defense/Scenes/controls_screen.gd
+++ sgj-tower-defense/Scenes/controls_screen.gd
@@ -10,11 +10,5 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/controls_screen.tscn")
-
-func _on_options_pressed() -> void:
-	pass # Replace with function body.
-
-func _on_quit_pressed() -> void:
-	get_tree().quit()
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/story1.tscn")
